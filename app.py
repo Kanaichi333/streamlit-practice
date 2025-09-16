c5e0560ec7199c902010ef22e6ad0037d693e346
Kanaichi333 <kaname.ih2023@gmail.com>
2025-09-16 11:12:42 +0900

    pages

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 
-tabs = ["Home", "掛け算", "3"]
-tab1, tab2, tab3 = st.tabs(tabs)
+tabs = ["Home", "掛け算", "カウント", "ラウンド"]
+tab1, tab2, tab3, tab4 = st.tabs(tabs)
 
 
 with tab1:
@@ -22,4 +22,30 @@ with tab2:
 
 
 with tab3:
-    st.title("3")
+    st.title("カウント")
+
+    # 初期化
+    if "count" not in st.session_state:
+        st.session_state.count = 0
+
+    # 更新
+    if st.button("カウントアップ"):
+        st.session_state.count += 1
+
+    # 表示
+    st.write("カウント:", st.session_state.count)
+
+
+
+
+with tab4:
+    st.title("ラウンド")
+
+    qp = st.query_params
+    round_num = int(qp.get("round", "1"))  # なければ 1
+    st.write(f"現在のラウンド: {round_num}")
+
+    if st.button("次のラウンドへ"):
+        round_num += 1
+        st.query_params["round"] = str(round_num)  # URLを更新
+        st.write(f"次のラウンドは {round_num} です")
